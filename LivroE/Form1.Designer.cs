df789c3ec9c12c08fd720ef6aa50665a3daaf862
valento45 <54119744+valento45@users.noreply.github.com>
2020-01-31 23:09:42 -0300

    Adicionar arquivos de projeto.

---
+++ LivroE/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace LivroE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabRegistro = new System.Windows.Forms.TabPage();
+            this.tabEmancipado = new System.Windows.Forms.TabPage();
+            this.tabInterditado = new System.Windows.Forms.TabPage();
+            this.tabCurador = new System.Windows.Forms.TabPage();
+            this.tabRegistroExterior = new System.Windows.Forms.TabPage();
+            this.tabOptante = new System.Windows.Forms.TabPage();
+            this.tabAdotado = new System.Windows.Forms.TabPage();
+            this.tabTutor = new System.Windows.Forms.TabPage();
+            this.tabEscritura = new System.Windows.Forms.TabPage();
+            this.tabSentença = new System.Windows.Forms.TabPage();
+            this.tabTranscricaoSentenca = new System.Windows.Forms.TabPage();
+            this.tabCasamentoTranslado = new System.Windows.Forms.TabPage();
+            this.tabPage11 = new System.Windows.Forms.TabPage();
+            this.pnlRegistro = new System.Windows.Forms.GroupBox();
+            this.pnlVariaveisAdicionais = new System.Windows.Forms.GroupBox();
+            this.tabControl1.SuspendLayout();
+            this.tabEmancipado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabRegistro);
+            this.tabControl1.Controls.Add(this.tabEmancipado);
+            this.tabControl1.Controls.Add(this.tabInterditado);
+            this.tabControl1.Controls.Add(this.tabCurador);
+            this.tabControl1.Controls.Add(this.tabRegistroExterior);
+            this.tabControl1.Controls.Add(this.tabOptante);
+            this.tabControl1.Controls.Add(this.tabAdotado);
+            this.tabControl1.Controls.Add(this.tabTutor);
+            this.tabControl1.Controls.Add(this.tabEscritura);
+            this.tabControl1.Controls.Add(this.tabSentença);
+            this.tabControl1.Controls.Add(this.tabTranscricaoSentenca);
+            this.tabControl1.Controls.Add(this.tabCasamentoTranslado);
+            this.tabControl1.Controls.Add(this.tabPage11);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1072, 652);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabRegistro
+            // 
+            this.tabRegistro.Location = new System.Drawing.Point(4, 22);
+            this.tabRegistro.Name = "tabRegistro";
+            this.tabRegistro.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRegistro.Size = new System.Drawing.Size(788, 365);
+            this.tabRegistro.TabIndex = 0;
+            this.tabRegistro.Text = "Registro";
+            this.tabRegistro.UseVisualStyleBackColor = true;
+            // 
+            // tabEmancipado
+            // 
+            this.tabEmancipado.Controls.Add(this.pnlVariaveisAdicionais);
+            this.tabEmancipado.Controls.Add(this.pnlRegistro);
+            this.tabEmancipado.Location = new System.Drawing.Point(4, 22);
+            this.tabEmancipado.Name = "tabEmancipado";
+            this.tabEmancipado.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEmancipado.Size = new System.Drawing.Size(1064, 626);
+            this.tabEmancipado.TabIndex = 1;
+            this.tabEmancipado.Text = "Emancipado";
+            this.tabEmancipado.UseVisualStyleBackColor = true;
+            // 
+            // tabInterditado
+            // 
+            this.tabInterditado.Location = new System.Drawing.Point(4, 22);
+            this.tabInterditado.Name = "tabInterditado";
+            this.tabInterditado.Padding = new System.Windows.Forms.Padding(3);
+            this.tabInterditado.Size = new System.Drawing.Size(788, 365);
+            this.tabInterditado.TabIndex = 2;
+            this.tabInterditado.Text = "Interditado";
+            this.tabInterditado.UseVisualStyleBackColor = true;
+            // 
+            // tabCurador
+            // 
+            this.tabCurador.Location = new System.Drawing.Point(4, 22);
+            this.tabCurador.Name = "tabCurador";
+            this.tabCurador.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCurador.Size = new System.Drawing.Size(788, 365);
+            this.tabCurador.TabIndex = 3;
+            this.tabCurador.Text = "Curador";
+            this.tabCurador.UseVisualStyleBackColor = true;
+            // 
+            // tabRegistroExterior
+            // 
+            this.tabRegistroExterior.Location = new System.Drawing.Point(4, 22);
+            this.tabRegistroExterior.Name = "tabRegistroExterior";
+            this.tabRegistroExterior.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRegistroExterior.Size = new System.Drawing.Size(788, 365);
+            this.tabRegistroExterior.TabIndex = 4;
+            this.tabRegistroExterior.Text = "Registro no Exterior";
+            this.tabRegistroExterior.UseVisualStyleBackColor = true;
+            // 
+            // tabOptante
+            // 
+            this.tabOptante.Location = new System.Drawing.Point(4, 22);
+            this.tabOptante.Name = "tabOptante";
+            this.tabOptante.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOptante.Size = new System.Drawing.Size(788, 365);
+            this.tabOptante.TabIndex = 5;
+            this.tabOptante.Text = "Optante";
+            this.tabOptante.UseVisualStyleBackColor = true;
+            // 
+            // tabAdotado
+            // 
+            this.tabAdotado.Location = new System.Drawing.Point(4, 22);
+            this.tabAdotado.Name = "tabAdotado";
+            this.tabAdotado.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAdotado.Size = new System.Drawing.Size(788, 365);
+            this.tabAdotado.TabIndex = 6;
+            this.tabAdotado.Text = "Adotado";
+            this.tabAdotado.UseVisualStyleBackColor = true;
+            // 
+            // tabTutor
+            // 
+            this.tabTutor.Location = new System.Drawing.Point(4, 22);
+            this.tabTutor.Name = "tabTutor";
+            this.tabTutor.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTutor.Size = new System.Drawing.Size(788, 365);
+            this.tabTutor.TabIndex = 7;
+            this.tabTutor.Text = "Tutor";
+            this.tabTutor.UseVisualStyleBackColor = true;
+            // 
+            // tabEscritura
+            // 
+            this.tabEscritura.Location = new System.Drawing.Point(4, 22);
+            this.tabEscritura.Name = "tabEscritura";
+            this.tabEscritura.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEscritura.Size = new System.Drawing.Size(788, 365);
+            this.tabEscritura.TabIndex = 8;
+            this.tabEscritura.Text = "Escritura";
+            this.tabEscritura.UseVisualStyleBackColor = true;
+            // 
+            // tabSentença
+            // 
+            this.tabSentença.Location = new System.Drawing.Point(4, 22);
+            this.tabSentença.Name = "tabSentença";
+            this.tabSentença.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSentença.Size = new System.Drawing.Size(788, 365);
+            this.tabSentença.TabIndex = 9;
+            this.tabSentença.Text = "Sentença";
+            this.tabSentença.UseVisualStyleBackColor = true;
+            // 
+            // tabTranscricaoSentenca
+            // 
+            this.tabTranscricaoSentenca.Location = new System.Drawing.Point(4, 22);
+            this.tabTranscricaoSentenca.Name = "tabTranscricaoSentenca";
+            this.tabTranscricaoSentenca.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTranscricaoSentenca.Size = new System.Drawing.Size(788, 365);
+            this.tabTranscricaoSentenca.TabIndex = 10;
+            this.tabTranscricaoSentenca.Text = "Transcrição da Sentença";
+            this.tabTranscricaoSentenca.UseVisualStyleBackColor = true;
+            // 
+            // tabCasamentoTranslado
+            // 
+            this.tabCasamentoTranslado.Location = new System.Drawing.Point(4, 22);
+            this.tabCasamentoTranslado.Name = "tabCasamentoTranslado";
+            this.tabCasamentoTranslado.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCasamentoTranslado.Size = new System.Drawing.Size(788, 365);
+            this.tabCasamentoTranslado.TabIndex = 11;
+            this.tabCasamentoTranslado.Text = "Casamento Translado";
+            this.tabCasamentoTranslado.UseVisualStyleBackColor = true;
+            // 
+            // tabPage11
+            // 
+            this.tabPage11.Location = new System.Drawing.Point(4, 22);
+            this.tabPage11.Name = "tabPage11";
+            this.tabPage11.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage11.Size = new System.Drawing.Size(788, 365);
+            this.tabPage11.TabIndex = 12;
+            this.tabPage11.Text = "tabPage11";
+            this.tabPage11.UseVisualStyleBackColor = true;
+            // 
+            // pnlRegistro
+            // 
+            this.pnlRegistro.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlRegistro.Location = new System.Drawing.Point(6, 6);
+            this.pnlRegistro.Name = "pnlRegistro";
+            this.pnlRegistro.Size = new System.Drawing.Size(1052, 407);
+            this.pnlRegistro.TabIndex = 0;
+            this.pnlRegistro.TabStop = false;
+            this.pnlRegistro.Text = "Registro";
+            // 
+            // pnlVariaveisAdicionais
+            // 
+            this.pnlVariaveisAdicionais.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlVariaveisAdicionais.Location = new System.Drawing.Point(6, 419);
+            this.pnlVariaveisAdicionais.Name = "pnlVariaveisAdicionais";
+            this.pnlVariaveisAdicionais.Size = new System.Drawing.Size(1052, 201);
+            this.pnlVariaveisAdicionais.TabIndex = 1;
+            this.pnlVariaveisAdicionais.TabStop = false;
+            this.pnlVariaveisAdicionais.Text = "Váriaveis Adicionais";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1096, 676);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Livro E";
+            this.tabControl1.ResumeLayout(false);
+            this.tabEmancipado.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabRegistro;
+        private System.Windows.Forms.TabPage tabEmancipado;
+        private System.Windows.Forms.GroupBox pnlRegistro;
+        private System.Windows.Forms.TabPage tabInterditado;
+        private System.Windows.Forms.TabPage tabCurador;
+        private System.Windows.Forms.TabPage tabRegistroExterior;
+        private System.Windows.Forms.TabPage tabOptante;
+        private System.Windows.Forms.TabPage tabAdotado;
+        private System.Windows.Forms.TabPage tabTutor;
+        private System.Windows.Forms.TabPage tabEscritura;
+        private System.Windows.Forms.TabPage tabSentença;
+        private System.Windows.Forms.TabPage tabTranscricaoSentenca;
+        private System.Windows.Forms.TabPage tabCasamentoTranslado;
+        private System.Windows.Forms.TabPage tabPage11;
+        private System.Windows.Forms.GroupBox pnlVariaveisAdicionais;
+    }
+}
+
